580ac0cc714d223b064d1438bad089d849910b5b
Kaustubh <kaustubhthakur76@gmail.com>
2026-08-22 23:04:12 +0530

    error fixed

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,46 @@
 const express = require("express");
 require("dotenv").config();
+
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const http = require("http");
-const PORT = process.env.PORT || 8081;
-const app = express();
-const path = require("path");
-const authrouter = require('./routes/auth')
-const userrouter = require('./routes/users')
-const storerouter = require('./routes/stores')
-const warehouserouter = require('./routes/warehouses')
-const shelfrouter = require('./routes/shelfs')
-const subshelfrouter = require('./routes/subshelf')
-const boxrouter = require('./routes/box');
 
-app.use(cors({origin: process.env.FRONTEND_URL || "http://localhost:3000",credentials: true, }));
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/users");
+const storeRouter = require("./routes/stores");
+const warehouseRouter = require("./routes/warehouses");
+const shelfRouter = require("./routes/shelfs");
+const subShelfRouter = require("./routes/subshelf");
+const boxRouter = require("./routes/box");
+
+const PORT = process.env.PORT || 8081;
+
+const app = express();
+
+app.use(
+  cors({
+    origin:
+      process.env.FRONTEND_URL ||
+      "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
-app.use('/auth',authrouter);
-app.use('/user',userrouter);
-app.use('/store',storerouter);
-app.use('/warehouse',warehouserouter)
-app.use('/subshelf',shelfrouter)
-app.use('/subshelf',subshelfrouter)
-app.use('./box',boxrouter);
+
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+
+app.use("/store", storeRouter);
+
+app.use("/warehouse", warehouseRouter);
+app.use("/shelf", shelfRouter);
+app.use("/subshelf", subShelfRouter);
+app.use("/box", boxRouter);
+
 const server = http.createServer(app);
 
-server.listen(PORT, () => {console.log(`Server running on port ${PORT}...`);});
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}...`);
+});
