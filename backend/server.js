eb04eddf7c1b56fdc579b298318eed6e22adc9c3
Kaustubh <kaustubhthakur76@gmail.com>
2026-08-22 18:00:20 +0530

    user+auth

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,14 @@ const http = require("http");
 const PORT = process.env.PORT || 8081;
 const app = express();
 const path = require("path");
+const authrouter = require('./routes/auth')
+const userrouter = require('./routes/users')
 
 app.use(cors({origin: process.env.FRONTEND_URL || "http://localhost:3000",credentials: true, }));
 app.use(express.json());
 app.use(cookieParser());
-
+app.use('/auth',authrouter);
+app.use('/user',userrouter);
 
 const server = http.createServer(app);
 
