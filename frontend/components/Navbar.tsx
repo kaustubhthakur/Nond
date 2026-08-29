e720b4c0edda4bbcaa9dc0c555081dc94a94acb9
Kaustubh <kaustubhthakur76@gmail.com>
2026-08-29 23:31:52 +0530

    name

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -103,7 +103,7 @@ export function Navbar() {
               href="/"
               className="font-display italic text-lg text-ink tracking-wide"
             >
-              Ledger
+              Nond
             </Link>
           )}
         </div>
