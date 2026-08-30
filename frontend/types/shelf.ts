a0a68c26658b2e70f5adcaf81b474b2fc4a61d18
Kaustubh <kaustubhthakur76@gmail.com>
2026-08-30 11:50:30 +0530

    shelf

---
+++ frontend/types/shelf.ts
@@ -0,0 +1,32 @@
+export interface Shelf {
+  id: string;
+  storeId: string;
+  warehouseId: string;
+
+  name: string;
+  description: string | null;
+
+  maxSubShelves: number;
+
+  productQuantity: number;
+  capacity: number;
+  availableCapacity: number;
+
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ShelfOptions {
+  maxSubShelves: number;
+  maxProducts: number;
+}
+
+export interface CreateShelfPayload {
+  name: string;
+  description?: string;
+}
+
+export interface UpdateShelfPayload {
+  name?: string;
+  description?: string;
+}
